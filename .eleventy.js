208d4608f86245d96176f1044063c5ebdb059b40
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-11-15 14:22:54 -0500

    fix dir name

--- .eleventy.js
+++ .eleventy.js
@@ -30,7 +30,7 @@ module.exports = function ( eleventyConfig ) {
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
-    "./src/well-known": "./well-known",
+    "./src/well-known": "./.well-known",
   } );
 
   // Copy Image Folder to /_site
