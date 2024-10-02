136a3940bb6d0b8c739b6957a53e9ceb4299feab
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-10-02 12:25:21 -0400

    biome lint/format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel(
-    {
-      webAnalytics: {
-        enabled: true,
-      }
-    }
-  ),
-});
+	integrations: [tailwind()],
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+})
