f264daf4f6b41d6737d0edc12f8e7562cf91a86d
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2025-04-24 16:08:07 -0400

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  site: 'https://joekarow.dev',
+  adapter: vercel()
 });
